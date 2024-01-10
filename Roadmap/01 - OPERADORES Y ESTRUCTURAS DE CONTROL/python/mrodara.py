1327c4dddb9bf741b4d7128a8acd63ae04011bc8
mrodara <manueljesus.rodriguez@iesalandalus.org>
2024-01-11 00:14:11 +0100

    reto 01 y 02 python mrodara

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/mrodara.py
@@ -0,0 +1,27 @@
+"""
+01 OPERADORES Y ESTRUCTURAS DE CONTROL
+  EJERCICIO:
+  - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+    Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+    (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+  - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+    que representen todos los tipos de estructuras de control que existan
+    en tu lenguaje:
+    Condicionales, iterativas, excepciones...
+  - Debes hacer print por consola del resultado de todos los ejemplos.
+ 
+  DIFICULTAD EXTRA (opcional):
+  Crea un programa que imprima por consola todos los números comprendidos
+  entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ 
+  Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+     
+"""
+
+def print_selected_numbers():
+    for i in range(10, 56):
+        if i % 2 == 0 and i != 16 and i % 3 != 0:
+            print(i)
+            
+            
+print_selected_numbers()
